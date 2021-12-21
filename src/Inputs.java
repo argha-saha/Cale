b6f7d109e890140d35a1ec1366ff68c4a5b4c20f
argha-saha <arghasaha@icloud.com>
2021-12-21 01:31:43 -0800

    Initial commit

---
+++ src/Inputs.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Inputs {
+	public static int getDate(Scanner scanner) {
+		System.out.println("Enter date [MMDDYY]: ");
+		return (scanner.nextInt());
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int date;
+		int month;
+		int year;
+		int day;
+		
+		int curMonth;
+		int dayNumber;
+		
+		int dayNumEnd;
+		int maxDays;
+		int isLeapYear;
+	}
+
+}
